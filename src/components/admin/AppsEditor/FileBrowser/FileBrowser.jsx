93a67856ca5f46b3ba973131ccef3d9b5a54b9e1
Wong Lok <wonglok@users.noreply.github.com>
2023-07-22 10:26:05 +0800

    fa

--- src/components/admin/AppsEditor/FileBrowser/FileBrowser.jsx
+++ src/components/admin/AppsEditor/FileBrowser/FileBrowser.jsx
@@ -103,6 +103,85 @@ export function FileBrowser() {
             )
           })}
 
+          <div className=''>
+            {appModules
+              .filter((r) => r.appPackageID === activePackageID)
+              .map((am) => {
+                return (
+                  <div key={am._id}>
+                    <div>{am.moduleName}</div>
+
+                    <div className='border-l pl-3'>
+                      <div
+                        className=' text-xs text-gray-500'
+                        onClick={async () => {
+                          //
+                          await useCodeGroups.getState().create({
+                            object: {
+                              appLoaderID: activeApp._id,
+                              appModuleID: am._id,
+                              groupName: 'newCodeGroups',
+                            },
+                          })
+
+                          await load({ activeApp })
+                        }}
+                      >
+                        + Add Code Group
+                      </div>
+
+                      {appCodeGroups
+                        .filter((acg) => {
+                          return acg.appModuleID === am._id
+                        })
+                        .map((acg) => {
+                          return (
+                            <div key={acg._id}>
+                              <div className=''>{acg.groupName}</div>
+
+                              <div className='border-l pl-3'>
+                                <div
+                                  className='text-xs text-gray-500'
+                                  onClick={async () => {
+                                    //
+                                    await useCodeFiles.getState().create({
+                                      object: {
+                                        appLoaderID: activeApp._id,
+                                        appCodeGroupID: acg._id,
+                                        fileName: 'newCodeFile',
+                                        content: '',
+                                      },
+                                    })
+
+                                    await load({ activeApp })
+                                  }}
+                                >
+                                  + Add Code File
+                                </div>
+
+                                <div className=''>
+                                  {appCodeFiles
+                                    .filter((acf) => {
+                                      return acf.appCodeGroupID === acg._id
+                                    })
+                                    .map((acf) => {
+                                      return (
+                                        <div className='' key={acf._id}>
+                                          <div className=''>{acf.fileName}</div>
+                                        </div>
+                                      )
+                                    })}
+                                </div>
+                              </div>
+                            </div>
+                          )
+                        })}
+                    </div>
+                  </div>
+                )
+              })}
+          </div>
+
           {/* {appPackages.map((ap) => {
             return (
               <div key={ap._id}>

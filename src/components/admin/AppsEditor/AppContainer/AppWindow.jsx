f5bc8675405fa424f2b87a4b1b7f55f5aaab501c
Wong Lok <wonglok@users.noreply.github.com>
2023-07-29 11:05:36 +0800

    app

--- src/components/admin/AppsEditor/AppContainer/AppWindow.jsx
+++ src/components/admin/AppsEditor/AppContainer/AppWindow.jsx
@@ -1,6 +1,25 @@
+import { useEffect } from 'react'
 import { useContainer } from './useContainer'
 
 export function AppWindow() {
   let iFrameSRC = useContainer((r) => r.iFrameSRC)
+
+  useEffect(() => {
+    let fnc = () => {
+      let backup = iFrameSRC
+      useContainer.setState({ iFrameSRC: 'about:blank' })
+      setTimeout(() => {
+        useContainer.setState({ iFrameSRC: backup })
+      }, 10)
+    }
+
+    window.addEventListener('savedFile', fnc)
+
+    return () => {
+      window.removeEventListener('savedFile', fnc)
+    }
+    ///
+  }, [iFrameSRC])
+
   return <>{<iframe className='h-full w-full' src={iFrameSRC || 'about:blank'}></iframe>}</>
 }

1c1ad610e6e3147a46f9953d613c6ccb90ff14be
Wong Lok <wonglok@users.noreply.github.com>
2023-08-06 17:59:32 +0800

    update slug

--- src/components/admin/URLs/UpdateSlug.jsx
+++ src/components/admin/URLs/UpdateSlug.jsx
@@ -81,10 +81,22 @@ export function UpdateSlug({ data }) {
                     return { ...st, slugs: st.slugs.filter((r) => r._id !== data._id) }
                   })
                 }}
-                className=' focus:shadow-outline mr-2 inline-block cursor-pointer rounded-r bg-red-200 px-4 py-2 font-bold text-white shadow hover:bg-red-400 focus:outline-none'
+                className='focus:shadow-outline mr-2 inline-block cursor-pointer rounded-r bg-red-200 px-4 py-2 font-bold text-white shadow hover:bg-red-400 focus:outline-none'
               >
                 ❌
               </button>
+            </div>
+          </div>
+
+          <div className='mb-3 md:flex md:items-center'>
+            <div className='md:w-1/3'>
+              <label className='mb-1 block pr-4 text-gray-500 md:mb-0 md:text-right' htmlFor='inline-full-name'>
+                <div className='inline-block text-sm'>Actions:</div>
+              </label>
+            </div>
+            <div className='md:w-2/3'>
+              <div className='flex'>123</div>
+              {/*  */}
             </div>
           </div>
 

aa01524bfec634b68f12eacb707b4c51fbe66c70
Wong Lok <wonglok@users.noreply.github.com>
2023-08-14 15:19:39 +0800

    fa

--- src/components/admin/Menu/MenuItems.jsx
+++ src/components/admin/Menu/MenuItems.jsx
@@ -33,7 +33,7 @@ export const getMenuItems = () => {
           >
             <path d='M22 24h-20v-24h14l6 6v18zm-7-23h-12v22h18v-16h-6v-6zm3 15v1h-12v-1h12zm0-3v1h-12v-1h12zm0-3v1h-12v-1h12zm-2-4h4.586l-4.586-4.586v4.586z' />
           </svg>
-          3D Pages
+          WebPages
         </>
       ),
     },
